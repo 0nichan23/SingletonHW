f31772cbee769eb3e14adea1dc2da07cfb41136f
0nichan23 <ontamam@gmail.com>
2021-11-11 17:42:18 +0200

    hw

---
+++ HW/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float wallet =0;
+            ConsoleKeyInfo choice;
+            while (true)
+            {
+                Console.WriteLine("how much money do you want to draw from the atm?");
+                Console.WriteLine("1 - 100");
+                Console.WriteLine("2 - 200");
+                Console.WriteLine("3 - 300");
+                Console.WriteLine("4 - 400");
+                Console.WriteLine("5 - more");
+                Console.WriteLine("=======================================================");
+                choice = Console.ReadKey(true);
+                switch (choice.Key)
+                {
+                    case ConsoleKey.D1:
+                        wallet += Singleton.Instance.DrawMoney(100);
+                        break;
+                    case ConsoleKey.D2:
+                        wallet +=  Singleton.Instance.DrawMoney(200);
+                        break;
+                    case ConsoleKey.D3:
+                        wallet +=  Singleton.Instance.DrawMoney(300);
+                        break;
+                    case ConsoleKey.D4:
+                        wallet +=  Singleton.Instance.DrawMoney(400);
+                        break;
+                    case ConsoleKey.D5:
+                        float custom = float.Parse(Console.ReadLine());
+                        wallet +=  Singleton.Instance.DrawMoney(custom);
+                        break;
+
+                    default:
+                        Console.WriteLine("invalid input");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ HW/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace singleton
+{
+    class Singleton
+    {
+        public static Singleton Instance;
+        public float MoneyInAtm;
+        private Singleton()
+        {
+            Instance = this;
+            MoneyInAtm = 10000000;
+        }
+
+        public float DrawMoney(float amount)
+        {
+            if (MoneyInAtm - amount >= 0)
+            {
+                Console.WriteLine("here are " + amount + " $");
+                MoneyInAtm -= amount;
+                return amount;
+            }
+            else
+            {
+                Console.WriteLine("not enough money in the atm");
+                return 0;
+            }
+        }
+    }
+}
